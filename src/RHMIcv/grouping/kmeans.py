8322b0d5d59d2446cd8467a72c239c2c38eda05c
Luritu <willi.grande@gmail.com>
2023-06-19 22:00:28 +0200

    Test für KMeans, konnte aber noch nicht validiert werden

--- src/RHMIcv/grouping/kmeans.py
+++ src/RHMIcv/grouping/kmeans.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import math
 
 import cv2
 
@@ -20,13 +21,28 @@ class KMeans(GroupingBase):
             new_points[i] = centroids[cluster[i]]
         return np.reshape(new_points, (image.shape[0], image.shape[1]))
 
-    def group(self, k, max_iterations=1000, dimension=2):
+    def group(self, k, max_iterations=1000, dimension=2, datapoints=None):
         # start ...
         print("Need to be implemented")
+        if datapoints == None:
+            return []
 
         # Limit iterations
+        self.cluster_centers = random.choice(datapoints, k)
+        self.cluster = np.arange(k)
+
+        for step in range(max_iterations):
+            for i in range(len(datapoints)):
+                idx = np.argmin(math.dist(self.cluster_centers, datapoints[i]))
+                self.cluster[idx].np.append(datapoints[i])
 
         # Randomly Choose Centers for the Clusters
 
         # ... end
-        return [cluster, np.array(cluster_centers)]
+        return [self.cluster, np.array(self.cluster_centers)]
+    
+if __name__ == "__init__":  # Konnte noch nicht getestet werden
+    KMeansAgent = KMeans()
+    datapoints = list(random.random() for i in range (20))
+    [cluster, cluster_centers] = KMeansAgent.group(3)
+    print(cluster_centers)
